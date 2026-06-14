cd45585baa7ce669abdae225362d743921dad349
hugobrisset <75309082+hugobrisset@users.noreply.github.com>
2026-06-14 16:56:49 +0200

    test http request

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+
+  healthData: any;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getHealth().subscribe({
+      next: (res) => {
+        console.log('Backend response:', res);
+        this.healthData = res;
+      },
+      error: (err) => {
+        console.error('Erreur API:', err);
+      }
+    });
+  }
 }
